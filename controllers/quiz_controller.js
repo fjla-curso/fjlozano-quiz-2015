b3f5191af053bfee467aa4e3f244d44ef4cce112
Javier Lozano <blesa.gaceta@gmail.com>
2015-07-16 10:55:33 +0200

    Despliegue en Heroku fjla

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -4,9 +4,16 @@ exports.question = function (req, res) {
 };
 
 exports.answer = function (req, res) {
-
-   if (req.query.respuesta === 'Madrid') {	
+   srespuesta = req.query.respuesta;
+   srespuesta = srespuesta.toUpperCase();
+   // aprende: esta línea lo hace fallar con undefined is not a function: srespuesta = srespuesta.allReplace({'Á': 'A', 'Ó': 'O', 'É': 'E', 'Í': 'I', 'Ú' : 'U', 'Ü': 'U' });
+   
+   if (srespuesta === 'MADRID') {	
 	res.render('quizes/answer', { respuesta: '¡Correcto!', title:"Quiz (a mano)"});
+   } else 
+   if (srespuesta === '') { 
+	// aprende: me muestra el código HTML si lo pongo aquí: res.render('quizes/answer', { respuesta: ' <span style="color:#F00">¿En blanco...? ¡Inténtalo de nuevo!</span> ', title:"Quiz (a mano)"});
+	res.render('quizes/answer', { respuesta: '¿En blanco...? ¡Inténtalo de nuevo! ', title:"Quiz (a mano)"});
    } else {
 	res.render('quizes/answer', { respuesta: '¡Oh, no es correcto!'  , title:"Quiz"  });
    }
